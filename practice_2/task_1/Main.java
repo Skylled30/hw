38f3abe5e2793e6575dfc55611c8c35f597fe48e
Alexandr Vanteev <vanteevs@mail.ru>
2022-06-06 20:57:12 +0300

    Create Main.java

---
+++ practice_2/task_1/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point point1 = new Point(10, 15);
+        Point point2 = new Point(20, 20);
+        System.out.println(point1.distanceTo(point2));
+    }
+}
